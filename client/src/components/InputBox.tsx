b256546f6236dd47709a208bbdd474686a32ff18
Phlegm Mignon <dmah@ucdavis.edu>
2025-04-13 20:50:05 -0700

    test cors

--- client/src/components/InputBox.tsx
+++ client/src/components/InputBox.tsx
@@ -26,7 +26,6 @@ const InputBox: React.FC = () => {
         // const res = await fetch("http://127.0.0.1:8000 ", {
         method: "POST",
         mode: "cors",
-        credentials: "include",
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "https://rag-project-1.onrender.com",
